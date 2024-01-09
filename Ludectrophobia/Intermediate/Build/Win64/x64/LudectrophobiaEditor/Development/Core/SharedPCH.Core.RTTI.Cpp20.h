eccc7ad165242f58d6adf2ca9345295c25590cf4
Lars <largon@uni-bremen.de>
2024-01-09 21:48:28 +0100

    Revert "C++ Klassen vorbereitet aber ohne Funktion selbst. 2 Interfaces und 3 Klassen sind als Gerüst bereit"
    This reverts commit 1d127a19622c858dc7d150d9cb22399ebf7978ca.

--- Ludectrophobia/Intermediate/Build/Win64/x64/LudectrophobiaEditor/Development/Core/SharedPCH.Core.RTTI.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Core/Public/CoreSharedPCH.h
-#include "C:/Users/melih/OneDrive/Uni Bremen/3. Semester/Media Engineering/ProgrammierSpiel/Ludectrophobia/Ludectrophobia/Intermediate/Build/Win64/x64/LudectrophobiaEditor/Development/Core/SharedDefinitions.Core.RTTI.Cpp20.h"
-#include "Runtime/Core/Public/CoreSharedPCH.h"
--- Ludectrophobia/Intermediate/Build/Win64/x64/LudectrophobiaEditor/Development/Slate/SharedPCH.Slate.Cpp20.h
+++
@@ -1,3 +0,0 @@
-// PCH for Runtime/Slate/Public/SlateSharedPCH.h
-#include "C:/Users/melih/OneDrive/Uni Bremen/3. Semester/Media Engineering/ProgrammierSpiel/Ludectrophobia/Ludectrophobia/Intermediate/Build/Win64/x64/LudectrophobiaEditor/Development/Slate/SharedDefinitions.Slate.Cpp20.h"
-#include "Runtime/Slate/Public/SlateSharedPCH.h"
